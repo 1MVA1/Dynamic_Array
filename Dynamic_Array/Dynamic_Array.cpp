5c480a2d1a4ce266d0bce5cb5a5cc61185ba2f23
1MVA1 <volodya-mazyar@yandex.ru>
2024-10-09 17:04:57 +0300

    Добавьте файлы проекта.

---
+++ Dynamic_Array/Dynamic_Array.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <new>        // Для operator new[]
+#include <stdexcept>  // Для invalid_argument
+
+using namespace std;
+
+template<typename T>
+class Array final 
+{
+
+private:
+    T* indicator;      
+    int size;
+    int current_size;
+    static const int default_size = 4;
+
+public:
+
+    Array() : Array(default_size) {}
+
+    Array(int size)
+    {
+        if (size <= 0) {
+            throw invalid_argument("Size must be positive.");
+        }
+
+        this.size = size;
+        current_size = 0;
+        indicator = static_cast<T*>(operator new[](size * sizeof(T)));
+    }
+
+    ~Array() 
+    {
+        for (int i = 0; i < current_size; ++i) {
+            indicator[i].~T();
+        }
+
+        operator delete[](indicator);
+    }
+
+    int insert(const T& value);
+
+    int insert(int index, const T& value);
+
+    const T& operator[](int index) const;
+
+    T& operator[](int index);
+
+    int size() const{
+        return current_size;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
